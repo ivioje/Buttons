3c06a28d16ab5ae4fd290b50ba8ea7921f1fd544
ivioje <rhodaivioje@gmail.com>
2021-01-19 17:16:36 +0100

    added counter

---
+++ src/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  state = {
+    count: 0
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className='btn btn-secondary  btn-sm'>Increment</button>
+      </React.Fragment>
+    )
+  }
+
+  formatCount = () => {
+    const { count } = this.state
+    return count === 0 ? 'zero' : count
+  }
+
+  getBadgeClasses () {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.count === 0 ? 'warning' : 'primary'
+    return classes
+  }
+}
+
+export default Counter
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Counter from './Counter';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+    <Counter />,
+  document.getElementById('root')
+);
+
+
