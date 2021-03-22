f94a5eed115c09bb402ee128995c35ea39164c07
ivioje <rhodaivioje@gmail.com>
2021-03-22 11:28:54 +0100

    delete counters

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,34 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    value: this.props.value
-  }
+    value: this.props.counter.value,
+  };
 
   handleIncrement = () => {
-    this.setState({value: this.state.value + 1})
-  }
+    this.setState({ value: this.state.value + 1 });
+  };
+  handleDecrement = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
   render () {
-  
     return (
       <div>
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={ this.handleIncrement} className='btn btn-secondary  btn-sm'>Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className='btn btn-primary m-2 btn-sm'
+        >
+          Decrement
+        </button>
+        <button className='btn btn-danger btn-sm m-2' onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
       </div>
-    )
+    );
   }
 
   formatCount = () => {
-    const { value } = this.state
-    return value === 0 ? 'zero' : value
-  }
+    const { value } = this.state;
+    return value === 0 ? 'zero' : value;
+  };
 
   getBadgeClasses () {
-    let classes = 'badge m-2 badge-'
-    classes += this.state.value === 0 ? 'warning' : 'primary'
-    return classes
+    let classes = 'badge m-2 badge-';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
+    return classes;
   }
 }
 
-export default Counter
+export default Counter;
--- src/components/Counters.js
+++ src/components/Counters.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import Counter from './Counter.js'
+import Counter from './Counter.js';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            {id: 1, value: 0},
-            {id: 2, value: 0},
-            {id: 3, value: 0},
-            {id: 4, value: 0}
-        ]
-     }
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
 
-    render() { 
-        return ( 
-            <div>
-          {this.state.counters.map(counter => <Counter key={counter.id} value= {counter.value}>
-              <h2>counter #{counter.id}</h2>
-              </Counter>
-              )}
-            </div>
-         );
-    }
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+  render () {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            onDelete={this.handleDelete}
+           counter={counter}
+          >
+            <h2>counter #{counter.id}</h2>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Counters;
