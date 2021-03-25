7af459fdff5ebc614644ee5d69ec08c3b68a3e1e
ivioje <rhodaivioje@gmail.com>
2021-03-25 01:01:43 +0100

    destructuring arguments

--- src/components/Counters.js
+++ src/components/Counters.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 import Counter from './Counter.js';
 
 class Counters extends Component {
-  render() {
-
+  render () {
+    const {
+      onReset,
+      onIncrement,
+      onDecrement,
+      onDelete,
+      counters,
+    } = this.props;
     return (
       <div>
-        <button onClick={this.props.onReset} className='btn btn-primary btn-sm m-2'>
+        <button onClick={onReset} className='btn btn-primary btn-sm m-2'>
           Reset
         </button>
-  
-        {this.props.counters.map(counter => (
+
+        {counters.map(counter => (
           <Counter
             key={counter.id}
             value={counter.value}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           >
             <h2>counter #{counter.id}</h2>
@@ -24,7 +30,7 @@ class Counters extends Component {
         ))}
       </div>
     );
-  };
   }
+}
 
 export default Counters;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-    render() {
-        return(
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="/">
-                    Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        )
-    }
+const Navbar = ({ totalCounters }) => { 
+
+    return (
+      <nav className='navbar navbar-light bg-light'>
+        <a className='navbar-brand' href='/'>
+          Navbar{' '}
+          <span className='badge badge-pill badge-secondary'>
+            {totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
 }
 export default Navbar;
