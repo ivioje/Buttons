0074a33003db5f5e64477c71f9f7be425193bce3
ivioje <rhodaivioje@gmail.com>
2021-03-25 01:06:22 +0100

    destructuring arguments

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  // state = {
-  //   value: this.props.counter.value,
-  // };
-
-  // handleIncrement = () => {
-  //   this.setState({ value: this.state.value + 1 });
-  // };
-  // handleDecrement = () => {
-  //   this.setState({ value: this.state.value - 1 });
-  // };
-
   render () {
+    const { onIncrement, onDecrement, onDelete, counter } = this.props;
     return (
       <div>
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
+          onClick={() => onIncrement(counter)}
           className='btn btn-secondary btn-sm'
         >
           Increment
         </button>
         <button
-          onClick={() => this.props.onDecrement(this.props.counter)}
+          onClick={() => onDecrement(counter)}
           className='btn btn-primary m-2 btn-sm'
         >
           Decrement
         </button>
         <button
           className='btn btn-danger btn-sm m-2'
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDelete(counter.id)}
         >
           Delete
         </button>
